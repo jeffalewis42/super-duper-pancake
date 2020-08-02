856a820fa64ac810f4d006bb3352b3522e62faaf
Jeffrey Lewis <jeffalewis42@gmail.com>
2020-08-01 19:48:46 -0500

    Page is finished still need backend

---
+++ javas.js
@@ -0,0 +1,30 @@
+//('#LargeVidModal').on('show.bs.modal', function (e) {
+//    var button = (e.relatedTarget); // Button that triggered the modal
+//
+//    var VidChoice = button.data('whatever'); // Extract info from data-* attributes
+//    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//    Console.log = VidChoice
+//    var VidCaption = document.getElementById('VidCaption');
+//    var VidUrls = [
+//      "ilzy0viqVes",
+//        "cywxQu_ZHYg",
+//        "NpG8NYVePCE"
+//  ];
+//    var VidCaptions = [
+//      "Kickflips are easy. Pop, flick, catch, land. #Boom",
+//        "360 Flips are easy too",
+//        "Backside Crooks are sick!"
+//  ]
+//    var VidChUrl = "https://www.youtube.com/embed/" + VidUrls[VidChoice] + "?rel=0";
+//    var VidText = VidCaptions[VidChoice];
+//    var modal = (this)
+//
+//    console.log = VidChUrl
+//    console.log = VidText
+//
+//    //  modal.find('.modal-title').text('New message to ' + recipient);
+//    //  modal.find('.modal-body input').val(recipient);
+//    Document.getElementById("VidIFrame").src = VidChUrl;
+//    document.getElementById("VidCaption").inner = VidText;
+//})
